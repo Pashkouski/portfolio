309767815ac126c4b3a52ef700881fbceb33a202
pashkouski <satiym@gmail.com>
2023-07-05 21:50:57 +0300

    3

--- src/ParticleBackground/ParticleBackground.tsx
+++ src/ParticleBackground/ParticleBackground.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import {useCallback} from "react";
 import {Container, Engine} from "tsparticles-engine";
 import {loadFull} from "tsparticles";
 import Particles from "react-tsparticles";
@@ -13,15 +13,19 @@ export const ParticleBackground = () => {
     }, []);
     return (
         <Particles
-            style={{opacity: 0.2}}
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
             options={{
                 background: {
                     color: {
-                        value: "#111",
+                        value: "#282834",
+
                     },
+                    image: "url('https://media.istockphoto.com/id/1294603953/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B0%D0%B1%D1%81%D1%82%D1%80%D0%B0%D0%BA%D1%82%D0%BD%D1%8B%D0%B5-%D1%87%D0%B5%D1%80%D0%BD%D1%8B%D0%B5-%D0%BF%D0%BE%D0%BB%D0%BE%D1%81%D1%8B-%D0%B4%D0%B8%D0%B0%D0%B3%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE-%D1%84%D0%BE%D0%BD%D0%B0.jpg?s=612x612&w=0&k=20&c=xkXkwxQ_3il-nWksI59D-XB9tpJyhuDyg5P39DSC1Ec=')",
+                    size: "cover", // задает масштабирование изображения
+                    repeat: "no-repeat", // задает повторение изображения
+                    position: "center center", // задает позицию изображения
                 },
                 fpsLimit: 120,
                 interactivity: {
@@ -78,13 +82,13 @@ export const ParticleBackground = () => {
                         value: 80,
                     },
                     opacity: {
-                        value: 0.5,
+                        value: 1,
                     },
                     shape: {
                         type: "circle",
                     },
                     size: {
-                        value: { min: 1, max: 5 },
+                        value: {min: 1, max: 5},
                     },
                 },
                 detectRetina: true,
